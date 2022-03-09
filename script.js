585c726241734849987599d872c098f34e76611d
Hobylicious <justiceguy88@gmail.com>
2022-03-08 20:52:24 -0500

    complete rework of html/css files; got colors to show on click; next task make them go away again

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-//make pairs for matching
-let pair1 = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4']
-let pair2 = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4']
+function cardClicked(e) {
+    const target = e.currentTarget;
+    console.log(e.target)
+    target.className = target.className.replace('hiding', '').trim();
 
-//assign pairs from p1+p2 to grid randomly
-//math.floor x math.random
-
-//eventListeners for clicking
-//flip card over on click
-//card a = card b : remove from board + add 2 points to scoreboard
-//card a != carb b :flip both cards back over
+}
